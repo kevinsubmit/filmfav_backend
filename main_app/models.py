69dc36d9b2dfb3b1d8e3bd526c80b3b9f237dd47
austin li <645267294@qq.com>
2025-01-18 14:50:37 -0500

    remove the created_at field

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,6 @@ class User(models.Model):
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
     password_hash = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
