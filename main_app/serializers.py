18f125bb19d6dd52723e55de686623764311c2c9
viededario <darioocceus@gmail.com>
2025-01-19 10:39:00 -0500

    serialzers, views and urls for Movies and Watchlist

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User # add this line to list of imports
-
+from .models import Movie, Genre, WatchList, WatchListMovie, Review, Comment
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)  # Add a password field, make it write-only
@@ -17,3 +17,78 @@ class UserSerializer(serializers.ModelSerializer):
       )
       
       return user
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['id', 'name']
+
+class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    genre_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Genre.objects.all(),
+        source='genres',
+        write_only=True,
+        required=False
+    )
+    average_rating = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Movie
+        fields = [
+            'id', 
+            'title', 
+            'description', 
+            'year_made', 
+            'poster_url', 
+            'genres', 
+            'genre_ids',
+            'average_rating', 
+            'created_at'
+        ]
+    
+    def get_average_rating(self, obj):
+        reviews = Review.objects.filter(movie=obj)
+        if reviews.exists():
+            return sum(float(review.rating) for review in reviews) / reviews.count()
+        return None
+
+class ReviewSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    movie_title = serializers.CharField(source='movie.title', read_only=True)
+    
+    class Meta:
+        model = Review
+        fields = ['id', 'username', 'movie_title', 'movie', 'text', 'rating', 'created_at']
+        read_only_fields = ['user']
+
+class CommentSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'username', 'review', 'text', 'created_at']
+        read_only_fields = ['user']
+
+class WatchListMovieSerializer(serializers.ModelSerializer):
+    movie = MovieSerializer(read_only=True)
+    movie_id = serializers.PrimaryKeyRelatedField(
+        queryset=Movie.objects.all(),
+        source='movie',
+        write_only=True
+    )
+    
+    class Meta:
+        model = WatchListMovie
+        fields = ['id', 'movie', 'movie_id']
+        read_only_fields = ['watch_list']
+
+class WatchListSerializer(serializers.ModelSerializer):
+    movies = WatchListMovieSerializer(source='watchlistmovie_set', many=True, read_only=True)
+    user = serializers.StringRelatedField(read_only=True)
+    
+    class Meta:
+        model = WatchList
+        fields = ['id', 'user', 'movies', 'added_at']
+        read_only_fields = ['user']
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import Home,CreateUserView,LoginView
+from .views import Home, CreateUserView, LoginView, MovieView,  MovieView, MovieDetailView, WatchListView, AddToWatchListView,RemoveFromWatchListView
 
 urlpatterns = [
     path('',Home.as_view(),name='home'),
     path('users/register/', CreateUserView.as_view(), name='register'),
     path('users/login/', LoginView.as_view(), name='login'),
+    path('movies/', MovieView.as_view(), name='movie-list'),
+    path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),  
+    path('watchlist/', WatchListView.as_view(), name='watchlist'),
+    path('watchlist/add/<int:pk>/', AddToWatchListView.as_view(), name='add-to-watchlist'),
+    path('watchlist/remove/<int:pk>/', RemoveFromWatchListView.as_view(), name='remove-from-watchlist'),
+
 ]
